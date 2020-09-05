6e98dbf693781ab4e67be46585348b13642df35e
melissa <nadia.mm.mariduena@gmail.com>
2020-09-05 21:42:45 +0200

    Updates

---
+++ precache-manifest.b2addad41215417661843ce0fcc80350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913c11634e968952164ab925a899e71f",
+    "url": "/react-json-version2/index.html"
+  },
+  {
+    "revision": "2e319fbf2935992bd55c",
+    "url": "/react-json-version2/static/css/main.08f367d5.chunk.css"
+  },
+  {
+    "revision": "a4f786cc504c17fa1792",
+    "url": "/react-json-version2/static/js/2.61cda704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-json-version2/static/js/2.61cda704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e319fbf2935992bd55c",
+    "url": "/react-json-version2/static/js/main.dc756ea1.chunk.js"
+  },
+  {
+    "revision": "5ff996e3d8fc1f5477ea",
+    "url": "/react-json-version2/static/js/runtime-main.98d8a98e.js"
+  }
+]);
